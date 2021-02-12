a2a9517f9d47541b68341860213cc1337e712202
Doğacan Biçer <doga.can.455@gmail.com>
2021-02-12 10:14:02 +0300

    Update index.js

--- MERN_CRUD_2/server/index.js
+++ MERN_CRUD_2/server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://dogacan:mustafa670@crud.hizbl.mongodb.net/memories?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://dogacan:<password>@crud.hizbl.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
