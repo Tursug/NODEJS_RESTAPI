4c2ee98d299fd06f32b7106b46226cadf3cff0fd
Doğacan Biçer <doga.can.455@gmail.com>
2021-02-12 10:50:40 +0300

    Update index.js

--- MERN_CRUD_1/server/index.js
+++ MERN_CRUD_1/server/index.js
@@ -2,40 +2,77 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const app = express();
+
 const FoodModel = require("./models/Food");
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
-mongoose.connect('mongodb+srv://dogacan:<password>@crud.hizbl.mongodb.net/<dbname>?retryWrites=true&w=majority', 
-{useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+       next();
+ });
+
+try{
+    mongoose.connect('mongodb+srv://dogacan:mustafa670@crud.hizbl.mongodb.net/crud?retryWrites=true&w=majority', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+    });
+    console.log("Connected");
+}catch(error){
+    console.error("Could Not Connect");
+}
 
 app.post('/insert', async (req, res) => {
 
-    const foodName = req.body.foodName;
-    const days = req.body.days;
+    const foodName = req.body.foodName
+    const days = req.body.days
 
-    const food = new FoodModel({foodName: foodName, daysSinceIAte: days});
+    const food = new FoodModel({foodName:foodName, daysSinceIAte: days});
 
-    
     try {
         await food.save();
-        res.send("inserted data");
-    }catch(err){
-        console.log(err)
+        res.send("Inserted Data");
+    } catch (error) {
+        console.error(error);
     }
-
 });
 
 app.get('/read', async (req, res) => {
-    FoodModel.find({},(err, result) => {
+    FoodModel.find({}, (err, result) => {
         if(err){
-            res.send(err)
+            res.send(err);
         }
-        res.send(result)
+
+        res.send(result);
     })
 });
 
-app.listen(3001, ()=>{
-    console.log("Server is running on port 3001");
+app.put('/update', async (req, res) => {
+
+    const newFoodName = req.body.newFoodName;
+    const id = req.body.id;
+
+    try {
+        await FoodModel.findById(id, (error, updatedFood)=>{
+            updatedFood.foodName = newFoodName;
+            updatedFood.save();
+            res.send("update");
+        })
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.delete('/delete/:id', async (req, res) => {
+    const id = req.params.id;
+
+    await FoodModel.findByIdAndRemove(id).exec();
+    res.send("deleted");
+});
+
+app.listen(3001, () => {
+    console.log("Server is up and running on port 3001");
 });
